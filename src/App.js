8548fd521b4573fdf2998ee83a2ee397d4f01d4a
iankhor <iankhorbc.thecoder@gmail.com>
2017-02-24 15:01:04 +1100

    pdated readme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactGridLayout from 'react-grid-layout' 
+
 
 class App extends Component {
   render() {
+
+    var layout = [
+      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+    ];
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="">
+        <div className="">
+          <h2>React Grid Layout playground</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+        <div key={'a'}>Static</div>
+        <div key={'b'}>Resizable</div>
+        <div key={'c'}>Resizable</div>
+      </ReactGridLayout>
+
       </div>
     );
   }
